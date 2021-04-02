cd493fd7859e99147c5d241ac887db7f5a180127
Riccardo <riccardo@null.net>
2021-04-02 20:57:44 +0200

    Task 4

--- write.py
+++ write.py
@@ -13,6 +13,8 @@ You'll edit this file in Part 4.
 import csv
 import json
 
+from helpers import datetime_to_str
+
 
 def write_to_csv(results, filename):
     """Write an iterable of `CloseApproach` objects to a CSV file.
@@ -26,6 +28,19 @@ def write_to_csv(results, filename):
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for elem in results:
+            d = {}
+            d["datetime_utc"] = datetime_to_str(elem.time)
+            d["distance_au"] = elem.distance
+            d["velocity_km_s"] = elem.velocity
+            d["designation"] = elem.neo.designation
+            d["name"] = elem.neo.name if elem.neo.name else 'None'
+            d["diameter_km"] = elem.neo.diameter if elem.neo.diameter else 'nan'
+            d["potentially_hazardous"] = 'True' if elem.neo.hazardous else 'False'
+            writer.writerow(d)
 
 
 def write_to_json(results, filename):
@@ -40,3 +55,18 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        l = []
+        for elem in results:
+            d = {}
+            d["datetime_utc"]            = datetime_to_str(elem.time)
+            d["distance_au"]             = elem.distance
+            d["velocity_km_s"]           = elem.velocity
+            neo = {}
+            neo["designation"]           = elem.neo.designation
+            neo["name"]                  = elem.neo.name
+            neo["diameter_km"]           = elem.neo.diameter
+            neo["potentially_hazardous"] = elem.neo.hazardous
+            d["neo"] = neo
+            l.append(d)
+        json.dump(l, outfile, indent=2)
